e5143f62f5562da782c5f6fbee6b150359446704
mauricelehmann <m.lehmann94@gmail.com>
2019-03-04 10:24:25 +0100

    fix comment

--- joueur.h
+++ joueur.h
@@ -2,7 +2,7 @@
  -----------------------------------------------------------------------------------
  Laboratoire : 03
  Fichier     : joueur.h
- Auteur(s)   : Maurice Lehmann,Farouk Ferchichi,Florian, Florian Schaufelberger
+ Auteur(s)   : Maurice Lehmann,Farouk Ferchichi, Florian Schaufelberger
  Date        : 01.03.2019
 
  But         : TODO
