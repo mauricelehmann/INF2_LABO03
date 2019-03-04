5763b3a40d9f0ae5f4f90ea17f6b0fb89d0bbdd7
spirits3 <flo_diablo@hotmail.com>
2019-03-04 11:52:09 +0100

    constantes globales

---
+++ constantesGlobales.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTES_GLOBALES_H
+#define CONSTANTES_GLOBALES_H
+
+const unsigned NOMBRE_FAMILLES = 7;
+const unsigned CARTES_PAR_FAMILLES = 4;
+const unsigned NOMBRE_JOUEURS = 4;
+const unsigned CARTES_PAR_JOUEURS = 5;
+
+#endif
