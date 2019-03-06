ec795d5592c3f800bb7a8c94c5f621aaa3fff402
spirits3 <flo_diablo@hotmail.com>
2019-03-06 11:23:49 +0100

    int to char pour cartes_par_famille

--- constantesGlobales.h
+++ constantesGlobales.h
@@ -2,7 +2,7 @@
 #define CONSTANTES_GLOBALES_H
 
 const unsigned NOMBRE_FAMILLES = 7;
-const unsigned CARTES_PAR_FAMILLES = 4;
+const char CARTES_PAR_FAMILLES = 'D';
 const unsigned NOMBRE_JOUEURS = 4;
 const unsigned CARTES_PAR_JOUEURS = 5;
 
